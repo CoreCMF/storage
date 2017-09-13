0d4600f637e45046b9509105036be8a3c2cf2d21
bigrocs <bigrocs@qq.com>
2017-09-13 09:29:36 +0800

    控制器默认table设计

--- src/Http/Controllers/Api/ConfigController.php
+++ src/Http/Controllers/Api/ConfigController.php
@@ -3,22 +3,48 @@
 namespace CoreCMF\Storage\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use CoreCMF\Core\Support\Http\Request as CoreRequest;
 use Illuminate\Container\Container;
 
 use App\Http\Controllers\Controller;
+use CoreCMF\Admin\Models\Config as adminConfig;
 use CoreCMF\Storage\Http\Models\Config;
 
 class ConfigController extends Controller
 {
     private $configModel;
 
-    public function __construct(Config $configPro){
-       $this->configModel = $configPro;
+    public function __construct(Config $configPro,adminConfig $adminConfigPro){
+         $this->configModel = $configPro;
+         $this->adminConfigModel = $adminConfigPro;
     }
-    public function index(CoreRequest $request)
+    public function index(Request $request)
     {
-        return [];
+        $pageSizes = $this->adminConfigModel->getPageSizes();
+        $config = resolve('builderModel')
+                            ->request($request)
+                            ->pageSize($this->adminConfigModel->getPageSize())
+                            ->getData($this->configModel);
+        $table = resolve('builderTable')
+                    ->data($config['model'])
+                    ->column(['prop' => 'id',         'label'=> 'ID',     'width'=> '55'])
+                    ->column(['prop' => 'disks',      'label'=> '磁盘',   'minWidth'=> '100'])
+                    ->column(['prop' => 'driver',     'label'=> '驱动',   'minWidth'=> '80'])
+                    ->column(['prop' => 'bucket',     'label'=> 'Bucket', 'minWidth'=> '120'])
+                    ->column(['prop' => 'domain',     'label'=> '域名',   'minWidth'=> '270'])
+                    ->column(['prop' => 'status',     'label'=> '状态',   'minWidth'=> '90','type' => 'status'])
+                    ->column(['prop' => 'rightButton','label'=> '操作',   'minWidth'=> '220','type' => 'btn'])
+                    ->topButton(['buttonType'=>'add',       'apiUrl'=> route('api.admin.system.config.add'),'title'=>'添加磁盘'])                         // 添加新增按钮
+                    ->topButton(['buttonType'=>'resume',    'apiUrl'=> route('api.admin.system.config.status')])                         // 添加启用按钮
+                    ->topButton(['buttonType'=>'forbid',    'apiUrl'=> route('api.admin.system.config.status')])                         // 添加禁用按钮
+                    ->topButton(['buttonType'=>'delete',    'apiUrl'=> route('api.admin.system.config.delete')])                         // 添加删除按钮
+                    ->rightButton(['buttonType'=>'edit',    'apiUrl'=> route('api.admin.system.config.edit')])                         // 添加编辑按钮
+                    ->rightButton(['buttonType'=>'forbid',  'apiUrl'=> route('api.admin.system.config.status')])                       // 添加禁用/启用按钮
+                    ->rightButton(['buttonType'=>'delete',  'apiUrl'=> route('api.admin.system.config.delete')])                       // 添加删除按钮
+                    ->pagination(['total'=>$config['total'], 'pageSize'=>$config['pageSize'], 'pageSizes'=>$pageSizes])
+                    ->searchTitle('请输入搜索内容')
+                    ->searchSelect(['disks'=>'磁盘','id'=>'ID','driver'=>'驱动'])
+                    ;
+        return resolve('builderHtml')->title('云存储管理器')->item($table)->response();
     }
     public function update(Request $request)
     {
